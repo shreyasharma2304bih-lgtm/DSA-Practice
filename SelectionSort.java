13ea4de0bd37ce2e84c5a75efab95de0d8506d87
shreyasharma2304bih-lgtm <shreyasharma2304.bih@gmail.com>
2026-06-10 21:42:51 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class SelectionSort {
+    public static void printArray(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 3, 1, 2};
+        // Selection Sort
+        for(int i=0; i<arr.length-1; i++){
+            int minIndex = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}
