8991920ac7964af40bece209275b6203821ff1f9
Shreya Sharma <shreyasharma2304.bih@gmail.com>
2026-06-16 19:39:58 +0530

    Add files via upload

---
+++ PermutationUsingRecursion.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PermutationUsingRecursion {
+    public static void permute(String str, int l, int r) {
+        if (l == r) {
+            System.out.println(str);
+        } else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i); // backtrack
+            }
+        }
+    }
+
+    public static String swap(String str, int i, int j) {
+        char[] arr = str.toCharArray();
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return new String(arr);
+    }
+}
